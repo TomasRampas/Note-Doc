7c37f09ede94bb4e91ac261c086ecb95d1517f9a
TomasRampas <rampas.tomas@gmail.com>
2017-06-06 14:17:40 +0200

    002 CoroutineCubeMovement

---
+++ CoroutineCubeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineCubeMovement : MonoBehaviour {
+
+    float speedCube = 2f;
+    Vector3 direction = new Vector3(0, -1f, 0);
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(CubeMovement());
+        }
+	}
+
+    IEnumerator CubeMovement()
+    {
+        while (transform.position.y > -10f)
+        {
+            Vector3 velocityCube = direction * speedCube;
+            Vector3 moveAmountCube = velocityCube * Time.deltaTime;
+            transform.position += moveAmountCube;
+            yield return null;
+        }
+        transform.position = Vector3.zero;
+    }
+}
