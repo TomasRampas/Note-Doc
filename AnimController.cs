29ce22249a37414bf93ec7487c992812d9343fd6
TomasRampas <rampas.tomas@gmail.com>
2017-06-23 10:54:32 +0200

    AnimController

---
+++ AnimController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+
+    public Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            anim.Play("BeamProjectile");
+        }
+	}
+}
